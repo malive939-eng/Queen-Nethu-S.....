5eeed1029574e15a270ffec7f5d959736ea1993a
malive939-eng <malive939@gmail.com>
2025-08-13 15:30:52 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "Queen-Nethu-Help/IMG-20250216-WA0140.jpg at main · malive939-eng/Queen-Nethu-Help https://github.com/malive939-eng/Queen-Nethu-Help/blob/main/IMG-20250216-WA0140.jpg",
               },
               caption: sid,
             });
@@ -129,4 +129,5 @@ process.on("uncaughtException", function (err) {
 });
 
 module.exports = router;
+
 
